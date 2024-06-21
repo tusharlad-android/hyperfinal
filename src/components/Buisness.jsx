7e3e07d1c1a0f785371ccca33432ead09f01ddf3
tusharlad-android <tusharlad5112002@gmail.com>
2024-06-21 15:38:23 +0530

    updated change

--- src/components/Buisness.jsx
+++ src/components/Buisness.jsx
@@ -13,14 +13,14 @@ const Buisness = () =>(
   
   <div className="border p-4 rounded-[20px]  text-center flex flex-col items-center justify-center h-full aspect-w-1 aspect-h-1  feature-card">
   
-  <img src={strategy} className='w-16 h-16 mb-5'/>
+  <img src={strategy} alt=''className='w-16 h-16 mb-5'/>
    <h6 className='text-xl text-poppins text-white font-bold '>STRATEGY</h6>
    <p className={`${styles.paragraph} mt-5`}>Research<br/>Analysis<br/>Positioning<br/>Purpose<br/>Market Strategy</p>
   </div>
   </div>
   <div className="w-full sm:w-1/2 md:w-1/3  p-4 ">
   <div className="border p-4 rounded-[20px] text-center flex flex-col items-center justify-center h-full aspect-w-1 aspect-h-1  feature-card">
-  <img src={branding} className='w-16 h-16 mb-5'/>
+  <img src={branding} alt='' className='w-16 h-16 mb-5'/>
    <h6 className='text-xl text-poppins text-white font-bold '>BRANDING</h6>
    <p className={`${styles.paragraph} mt-5`}>Identity<br/>Voice<br/>Positioning<br/>Customer Journey<br/>Brand Guidelines</p>
   </div>
@@ -28,7 +28,7 @@ const Buisness = () =>(
   </div>
   <div className="w-full sm:w-1/2 md:w-1/3  p-4 ">
   <div className="border p-4 rounded-[20px] text-center flex flex-col items-center justify-center h-full aspect-w-1 aspect-h-1  feature-card">
-    <img src={content} className='w-16 h-16 mb-5'/>
+    <img src={content} alt='' className='w-16 h-16 mb-5'/>
    <h6 className='text-xl text-poppins text-white font-bold '>CONTENT</h6>
    <p className={`${styles.paragraph} mt-5`}>Video<br/>Photography<br/>Animations<br/>CopyWriting<br/>Social Content</p>
   </div>
@@ -36,7 +36,7 @@ const Buisness = () =>(
   </div>
   <div className="w-full sm:w-1/2 md:w-1/3  p-4 ">
   <div className="border p-4 rounded-[20px] text-center flex flex-col items-center justify-center h-full aspect-w-1 aspect-h-1  feature-card">
-  <img src={design} className='w-16 h-16 mb-5'/>
+  <img src={design} alt='' className='w-16 h-16 mb-5'/>
    <h6 className='text-xl text-poppins text-white font-bold '>DESIGN</h6>
    <p className={`${styles.paragraph} mt-5`}> App Design<br/>Website Design<br/>Traditional Design<br/>Experience Design<br/>Market Strategy</p>
   </div>
@@ -45,14 +45,14 @@ const Buisness = () =>(
   <div className="w-full sm:w-1/2 md:w-1/3  p-4 ">
   
   <div className="border p-4 rounded-[20px] text-center flex flex-col items-center justify-center h-full aspect-w-1 aspect-h-1  feature-card">
-  <img src={marketing} className='w-16 h-16 mb-5'/>
+  <img src={marketing} alt='' className='w-16 h-16 mb-5'/>
    <h6 className='text-xl text-poppins text-white font-bold '>MARKETING</h6>
    <p className={`${styles.paragraph} mt-5`}>Banner Ads & Retargeting<br/>CRM<br/>Email Campaigns<br/>Social Campaigns<br/>Influencer Marketing</p>
   </div>
   </div>
   <div className="w-full sm:w-1/2 md:w-1/3  p-4 ">
   <div className="border p-4 rounded-[20px] text-center flex flex-col items-center justify-center h-full aspect-w-1 aspect-h-1 feature-card">
-  <img src={technology} className='w-16 h-16 mb-5'/>
+  <img src={technology} alt='' className='w-16 h-16 mb-5'/>
    <h6 className='text-xl text-poppins text-white font-bold '>TECHNOLOGY</h6>
    <p className={`${styles.paragraph} mt-5`}>Content Management Systems<br/>Website Development<br/>App Development<br/>E-Commerce<br/>Market Strategy</p>
   </div>
